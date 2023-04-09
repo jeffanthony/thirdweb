6c79823aebc2b2330feac692f0c408f576dd3963
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2023-04-09 14:49:14 -0400

    Create middleManagement.sol

---
+++ contracts/extension/middleManagement.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BUSL-1.0
+
+// This is different from Owner and/or Permissions in the following ways:
+// 1) This will allow any tokenId to be managed by an independent third party separate from the others.
+// 2) this will be managed incentively according to BRANDS so that Brand managers can better navigate their tokenIds
+// 3) this allows the platform fee to go to the Contract owner while the remaining token sales go to their respective owners
