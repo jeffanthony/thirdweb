d6c6d5887e3c02f709bf3b9df574a9f04da6ed47
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2023-04-09 15:04:23 -0400

    Create upgradeableERC721.sol

---
+++ contracts/custom/upgradeableERC721.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "../node_modules/@thirdweb-dev/contracts/base/ERC721Drop.sol";
+import "../node_modules/@thirdweb-dev/contracts/extension/Upgradeable.sol";
+import "../node_modules/@thirdweb-dev/contracts/extension/Initializable.sol";
+
+
+contract UpgradeableERC1155 is Upgradeable, Initializable, 
+ERC721Drop("UpgradeableERC721", "U721", msg.sender, 130, msg.sender) {
+    address public deployer;
+	constructor() initializer {}
+
+	function initialize() external initializer {deployer = msg.sender;}
+
+	function _authorizeUpgrade(address) internal view override onlyOwner{
+		require(msg.sender == deployer);
+	}
+}
